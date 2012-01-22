a61bdf124329c148c4667fb462736c111c868def
Yuki Nagato <foxnewsnetwork@gmail.com>
2012-01-21 16:18:54 -0800

    initial commit

---
+++ veryangrybirds/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Veryangrybirds::Application.config.secret_token = 'c3f873698874a0deab19085ee7e1a70513efd186b55e1b3fe5843ea545bc23203bd46339672a245f042e06821e7978ff1333f2f19afaf495eadce26f4d8b23aa'
