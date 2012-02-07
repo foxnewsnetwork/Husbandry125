fde120c7215b523b9e5435b243a029f8cd761d09
foxnewsnetwork <foxnewsnetwork@gmail.com>
2012-02-06 17:35:09 -0800

    fixed the mibbuRotateSprite function, but have yet to implement it

--- controllers/extensions_controller.js
+++ controllers/extensions_controller.js
@@ -16,9 +16,15 @@ function mibbuMoveSpritePosition( sprite, dx, dy, dz){
 }
 
 // This function rotates the entire canvas, so unless you really need to call it, don't use it
+// 
 function mibbuRotateSprite( sprite, angle ){
-	sprite.angle = angle;
-	sprite.rotate(angle);
+	// Everything starts out at the 180 degree mark in terms of png views
+	// But angles are measured from the 0 degree 
+	// from 0->180 degrees covers 13 tiles
+	var quantumDegree = 13.8462;
+	var frameNumber = floor( angle / quantumDegree );
+	
+	sprite.frame(frameNumber);
 }
 
 function box2dMoveObject( body, dx, dy ){
