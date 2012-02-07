11e3620ea6b0dc382eef33d0c159b402ac35c728
foxnewsnetwork <foxnewsnetwork@gmail.com>
2012-02-07 13:22:28 -0800

    made it so that pigs turn around when you tell them to move in the direction behind them

--- controllers/ui_controller.js
+++ controllers/ui_controller.js
@@ -3,8 +3,8 @@
 var rightButton = new Game.spr('views/rightbutton.png', MOVE_BUTTON_WIDTH, MOVE_BUTTON_HEIGHT, 1, 0);
 var leftButton = new Game.spr('views/leftbutton.png', MOVE_BUTTON_WIDTH, MOVE_BUTTON_HEIGHT, 1, 0);
 
-mibbuSetSpritePosition( rightButton, 510, 300, Z_GUI);
-mibbuSetSpritePosition( leftButton, 15, 300, Z_GUI);
+mibbuSetSpritePosition( rightButton, GAME_WIDTH - 95, GAME_HEIGHT - 400, Z_GUI);
+mibbuSetSpritePosition( leftButton, 15, GAME_HEIGHT - 400, Z_GUI);
 
 rightButton.speed(0);
 leftButton.speed(0);
--- models/angrybarn.js
+++ models/angrybarn.js
@@ -39,28 +39,42 @@ var AngryBarn = function(world, player, barnDef) {
 	this.absX;
 	this.absY;
 	this.absZ;
+	this.direction;
 	
 	// animation
-	this.sprite = new Game.spr('views/pigbig.png', BARN_WIDTH, BARN_HEIGHT, 3, 0);
+	this.sprite2 = new Game.spr('views/RPigs.png', BARN_WIDTH, BARN_HEIGHT, 24, 0);
+	this.sprite = new Game.spr('views/LPigs.png', BARN_WIDTH, BARN_HEIGHT, 24, 0);
 	this.sprite.width = BARN_WIDTH;
 	this.sprite.height = BARN_HEIGHT;
-	this.cannonSprite = new Game.spr('views/cannon.png', CANNON_WIDTH, CANNON_HEIGHT, 1, 0);
+	this.sprite2.width = BARN_WIDTH;
+	this.sprite2.height = BARN_HEIGHT;
+	this.cannonSprite2 = new Game.spr('views/RCannons.png', CANNON_WIDTH, CANNON_HEIGHT, 24, 0);
+	this.cannonSprite = new Game.spr('views/LCannons.png', CANNON_WIDTH, CANNON_HEIGHT, 24, 0);
 	this.cannonSprite.width = CANNON_WIDTH;
 	this.cannonSprite.height = CANNON_HEIGHT;
+	this.cannonSprite2.width = CANNON_WIDTH;
+	this.cannonSprite2.height = CANNON_HEIGHT;
 	// Game world connection functions
 	this.initialize = function(x,y) {
 		this.absX = x;
 		this.absY = y;
-		this.cannonSprite.absX = x + 10;
+		this.cannonSprite.absX = x;
 		this.cannonSprite.absY = y - CANNON_HEIGHT;
+		this.direction = -1;
 		
 		mibbuSetSpritePosition( this.sprite, x, y, Z_CHARACTERS);
-		mibbuSetSpritePosition( this.cannonSprite, x+10, y-CANNON_HEIGHT, Z_CHARACTERS);
+		mibbuSetSpritePosition( this.cannonSprite, x, y-CANNON_HEIGHT, Z_CHARACTERS);
+		mibbuSetSpritePosition( this.sprite2, x, y, Z_CHARACTERS);
+		mibbuSetSpritePosition( this.cannonSprite2, x, y-CANNON_HEIGHT, Z_CHARACTERS);
 		
+		this.cannonSprite2.frame(25);
+		this.sprite2.frame(25);
 		this.cannonSprite.speed(0);
-		this.sprite.speed(7);
+		this.sprite.speed(0);
+		this.cannonSprite2.speed(0);
+		this.sprite2.speed(0);
 		this.conflux();
-		this.ammo.initialize(x+30, y - BARN_HEIGHT);
+		this.ammo.initialize(x+5, y - BARN_HEIGHT);
 	}
 	
 	// Game mechanics macros 
@@ -70,7 +84,7 @@ var AngryBarn = function(world, player, barnDef) {
 		this.absX = x;
 		this.absY = y;
 		mibbuSetSpritePosition( this.sprite, x, y, this.sprite.z );
-		mibbuSetSpritePosition( this.cannonSprite, x+10, y-CANNON_HEIGHT, this.sprite.z );
+		mibbuSetSpritePosition( this.cannonSprite, x, y-CANNON_HEIGHT, this.sprite.z );
 		this.conflux();
 	}
 	this.MovePosition = function(x,y){ 
@@ -84,6 +98,10 @@ var AngryBarn = function(world, player, barnDef) {
 //		mibbuMoveSpritePosition( this.sprite, direction * MOVE_SPEED, 0, 0);
 //		mibbuMoveSpritePosition( this.cannonSprite, direction * MOVE_SPEED, 0, 0);
 //		this.absX += direction * MOVE_SPEED;
+
+		// We manage the issue with the pigs facing direction here
+		this.switchDirection( this.direction != direction );
+		this.direction = direction;
         if(this.body.GetLinearVelocity().y == 0)
         {
             this.body.ApplyForce(new b2Vec2(0,.5),this.body.GetWorldCenter());
@@ -98,11 +116,30 @@ var AngryBarn = function(world, player, barnDef) {
         this.conflux();
         this.ammo.conflux();
 	}
-    
+    this.switchDirection = function( flag ){ 
+		if(flag) {
+			var temp = this.sprite;
+			this.sprite = this.sprite2;
+			this.sprite2 = temp;
+			
+			this.sprite.frame(0);
+			this.sprite2.frame(25);
+			
+			temp = this.cannonSprite;
+			this.cannonSprite = this.cannonSprite2;
+			this.cannonSprite2 = temp;
+			
+			this.cannonSprite.frame(0);
+			this.cannonSprite2.frame(25);
+		}
+	}
 	this.confluence = function(){
 		Confluence( this.body, this.sprite );
 		Confluence( this.body, this.cannonSprite );
-		mibbuMoveSpritePosition( this.cannonSprite,10,-CANNON_HEIGHT,0);
+		Confluence( this.body, this.sprite2 );
+		Confluence( this.body, this.cannonSprite2 );
+		mibbuMoveSpritePosition( this.cannonSprite,0,-CANNON_HEIGHT,0);
+		mibbuMoveSpritePosition( this.cannonSprite2,0,-CANNON_HEIGHT,0);
 	}
 	this.conflux = function(){
 		Conflux( this.body, this.sprite );
